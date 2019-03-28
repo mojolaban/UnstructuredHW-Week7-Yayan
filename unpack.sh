f2638ea3a05fd9b4e43371f9589d07db0a4c868e
mojolaban <44718604+mojolaban@users.noreply.github.com>
2019-03-28 23:21:07 +0000

    Update unpack.sh

--- unpack.sh
+++ unpack.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for f in -C ~/work/week6/cwl-data/data/structured/*.tar.gz
+for f in -C ~/work/week6/cwl-data/data/structured/structured-2018*.tar.gz
 do
         tar zxvf "$f" -C /home/vagrant/work/week7
 done
